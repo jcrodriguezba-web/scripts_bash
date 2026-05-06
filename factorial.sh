1c29aaeae314b2209434fa9c41270f09419f28d7
JuanC <jcrodriguezba@esliceu.net>
2026-05-06 16:06:19 +0200

    factorial y sumatorio

---
+++ factorial.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#factorial de un número
+
+if [[ $# -ne 1 ]]
+    echo "Error: se requiere un único parámetri"
+    exit 1
+fi
+
+#comprovar que realmente tenemos un número
+if ! [["$1" =~ ^[0-9]+$ ]]; then
+    echo "error requiere número"
+    exit 1
+fi
+
+n=$1
+echo "vamos a calcular el factorial de $n"
+#recoger el valor de n
+n=$1
+producto=1
+
+#bucle for
+for (( i=1;i<=n;i++ ))
+do
+    producto=$((producto*i))
+done
+
+echo "el factorial de $n es $producto"
---
+++ sumatorio.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+
+if [[ $# -ne 1 ]]
+    echo "Error: se requiere un único parámetri"
+    exit 1
+fi
+
+#comprovar que realmente tenemos un número
+if ! [["$1" =~ ^[0-9]+$ ]]; then
+    echo "error requiere número"
+    exit 1
+fi
+
+echo "Vamos a calcular el sumatorio de $1"
+
+#recoger el valor de n
+n=$1
+suma=0
+
+#bucle for
+for (( i=1;i<=n;i++ ))
+do
+    suma=$((suma+i))
+done
+
+echo "el sumatorio de $n vale $suma"
+#calcular sumatorio de un numero
+exit 0
